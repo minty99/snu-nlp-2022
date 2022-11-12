f20109d6f25fbefda59280386cb8660065ab073d
Changmin Choi <cmchoi9901@gmail.com>
2022-11-12 23:22:03 +0900

    move get_kobert_model to utils

--- kobert.py
+++ kobert.py
@@ -20,37 +20,10 @@ import sentencepiece as sp
 import torch
 from transformers import BertModel
 
-from utils.kobert_utils import download, get_tokenizer
+from utils.utils import get_pytorch_kobert_model
 
 
-def get_pytorch_kobert_model(ctx="cpu", cachedir=".cache"):
-    def get_kobert_model(model_path, vocab_file, ctx="cpu"):
-        bertmodel = BertModel.from_pretrained(model_path, return_dict=False)
-        device = torch.device(ctx)
-        bertmodel.to(device)
-        bertmodel.eval()
-        vocab = sp.SentencePieceProcessor()
-        vocab.load(vocab_file)
-        return bertmodel, vocab
-
-    pytorch_kobert = {
-        "url": "s3://skt-lsl-nlp-model/KoBERT/models/kobert_v1.zip",
-        "chksum": "411b242919",  # 411b2429199bc04558576acdcac6d498
-    }
-
-    # download model
-    model_info = pytorch_kobert
-    model_path, is_cached = download(model_info["url"], model_info["chksum"], cachedir=cachedir)
-    cachedir_full = os.path.expanduser(cachedir)
-    zipf = ZipFile(os.path.expanduser(model_path))
-    zipf.extractall(path=cachedir_full)
-    model_path = os.path.join(os.path.expanduser(cachedir), "kobert_from_pretrained")
-    # download vocab
-    vocab_path = get_tokenizer()
-    return get_kobert_model(model_path, vocab_path, ctx)
-
-
-if __name__ == "__main__":
+def main():
     input_ids = torch.LongTensor([[31, 51, 99], [15, 5, 0]])
     input_mask = torch.LongTensor([[1, 1, 1], [1, 1, 0]])
     token_type_ids = torch.LongTensor([[0, 0, 1], [0, 1, 0]])
@@ -59,3 +32,7 @@ if __name__ == "__main__":
     print(pooled_output.shape)
     print(vocab)
     print(sequence_output[0])
+
+
+if __name__ == "__main__":
+    main()
--- utils/utils.py
+++ utils/utils.py
@@ -1,12 +1,18 @@
 import logging
+import os
 import random
 import subprocess
 from datetime import datetime
+from zipfile import ZipFile
 
 import numpy as np
+import sentencepiece as sp
 import torch
 import torch.distributed as dist
 from omegaconf import OmegaConf
+from transformers import BertModel
+
+from utils.kobert_utils import download, get_tokenizer
 
 
 def set_random_seed(seed):
@@ -23,7 +29,32 @@ def is_logging_process():
 def get_logger(cfg, name=None):
     # log_file_path is used when unit testing
     if is_logging_process():
-        logging.config.dictConfig(
-            OmegaConf.to_container(cfg.job_logging_cfg, resolve=True)
-        )
+        logging.config.dictConfig(OmegaConf.to_container(cfg.job_logging_cfg, resolve=True))
         return logging.getLogger(name)
+
+
+def get_pytorch_kobert_model(ctx="cpu", cachedir=".cache"):
+    def get_kobert_model(model_path, vocab_file, ctx="cpu"):
+        bertmodel = BertModel.from_pretrained(model_path, return_dict=False)
+        device = torch.device(ctx)
+        bertmodel.to(device)
+        bertmodel.eval()
+        vocab = sp.SentencePieceProcessor()
+        vocab.load(vocab_file)
+        return bertmodel, vocab
+
+    pytorch_kobert = {
+        "url": "s3://skt-lsl-nlp-model/KoBERT/models/kobert_v1.zip",
+        "chksum": "411b242919",  # 411b2429199bc04558576acdcac6d498
+    }
+
+    # download model
+    model_info = pytorch_kobert
+    model_path, is_cached = download(model_info["url"], model_info["chksum"], cachedir=cachedir)
+    cachedir_full = os.path.expanduser(cachedir)
+    zipf = ZipFile(os.path.expanduser(model_path))
+    zipf.extractall(path=cachedir_full)
+    model_path = os.path.join(os.path.expanduser(cachedir), "kobert_from_pretrained")
+    # download vocab
+    vocab_path = get_tokenizer()
+    return get_kobert_model(model_path, vocab_path, ctx)
