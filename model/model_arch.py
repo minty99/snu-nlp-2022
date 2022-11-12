023451574d871ee725191d0a445afc142533100d
Muhwan Kim <mhkim4886@gmail.com>
2022-11-13 05:33:11 +0900

    Increase number of layers & Disable dropout

--- model/model_arch.py
+++ model/model_arch.py
@@ -10,16 +10,25 @@ class Net_arch(nn.Module):
         super(Net_arch, self).__init__()
         self.cfg = cfg
         self.bert, _ = get_pytorch_kobert_model(ctx=cfg.device)
-        self.fc = nn.Linear(768, 2)
+        self.fc1 = nn.Linear(768, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 2)
         for param in self.parameters():
             param.requires_grad = False
-        for param in self.fc.parameters():
+        for param in self.fc1.parameters():
+            param.requires_grad = True
+        for param in self.fc2.parameters():
+            param.requires_grad = True
+        for param in self.fc3.parameters():
             param.requires_grad = True
 
     def forward(self, x):
         # n_words = 512 (max)
         x, _ = self.bert(*x)  # [N, n_words, 768]
-        F.dropout(x, 0.5, training=self.training, inplace=True)
-        x = self.fc(x)  # [N, n_words, 768] -> [N, n_words, 2]
-
+        # F.dropout(x, 0.5, training=self.training, inplace=True)
+        x = self.fc1(x)  # [N, n_words, 768] -> [N, n_words, 256]
+        x = F.relu(x)
+        x = self.fc2(x)  # [N, n_words, 256] -> [N, n_words, 64]
+        x = F.relu(x)
+        x = self.fc3(x)  # [N, n_words, 64] -> [N, n_words, 2]
         return x  # [N, n_words, 2]
