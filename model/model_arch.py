f808b37865413a189c5aed708a221bd81fef14ab
Muhwan Kim <mhkim4886@gmail.com>
2022-11-12 23:48:55 +0900

    Implement model architecture

--- model/model_arch.py
+++ model/model_arch.py
@@ -1,6 +1,7 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from kobert import get_pytorch_kobert_model
 
 
 class Net_arch(nn.Module):
@@ -8,17 +9,13 @@ class Net_arch(nn.Module):
     def __init__(self, cfg):
         super(Net_arch, self).__init__()
         self.cfg = cfg
+        self.bert, _ = get_pytorch_kobert_model(ctx=cfg.device)
+        self.fc = nn.Linear(768, 2)
 
-        # TODO: This is example code. You should change this part as you need
-        self.lrelu = nn.LeakyReLU()
-        self.conv1 = nn.Sequential(nn.Conv2d(1, 4, 3, 2, 1), self.lrelu)
-        self.conv2 = nn.Sequential(nn.Conv2d(4, 4, 3, 2, 1), self.lrelu)
-        self.fc = nn.Linear(7 * 7 * 4, 10)
+    def forward(self, x):
+        # n_words = 512 (max)
+        x, _ = self.bert(x)  # [N, n_words, 768]
+        F.dropout(x, 0.5, training=self.training, inplace=True)
+        x = self.fc(x)  # [N, n_words, 768] -> [N, n_words, 2]
 
-    def forward(self, x):  # x: (B,1,28,28)
-        # TODO: This is example code. You should change this part as you need
-        x = self.conv1(x)  # x: (B,4,14,14)
-        x = self.conv2(x)  # x: (B,4,7,7)
-        x = torch.flatten(x, 1)  # x: (B,4*7*7)
-        x = self.fc(x)  # x: (B,10)
-        return x
+        return x  # [N, n_words, 2]
