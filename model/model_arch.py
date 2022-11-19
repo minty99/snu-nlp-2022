2dbeeafb7adc55cd816e755e842442cf85d9cbe7
Muhwan Kim <mhkim4886@gmail.com>
2022-11-20 05:51:15 +0900

    Revert to 3 FC layers & Enable gradients for BERT

--- model/model_arch.py
+++ model/model_arch.py
@@ -10,35 +10,27 @@ class Net_arch(nn.Module):
         super(Net_arch, self).__init__()
         self.cfg = cfg
         self.bert, _ = get_pytorch_kobert_model(ctx=cfg.device)
-        self.fc1 = nn.Linear(768, 4096)
-        self.fc2 = nn.Linear(4096, 1024)
-        self.fc3 = nn.Linear(1024, 128)
-        self.fc4 = nn.Linear(128, 32)
-        self.fc5 = nn.Linear(32, 2)
-        for param in self.parameters():
-            param.requires_grad = False
-        for param in self.fc1.parameters():
-            param.requires_grad = True
-        for param in self.fc2.parameters():
-            param.requires_grad = True
-        for param in self.fc3.parameters():
-            param.requires_grad = True
+        self.fc1 = nn.Linear(768, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 2)
+        # for param in self.parameters():
+        #     param.requires_grad = False
+        # for param in self.fc1.parameters():
+        #     param.requires_grad = True
+        # for param in self.fc2.parameters():
+        #     param.requires_grad = True
+        # for param in self.fc3.parameters():
+        #     param.requires_grad = True
 
     def forward(self, x):
         # n_words = 512 (max)
-        x, _ = self.bert(*x)
+        x, _ = self.bert(*x)  # [N, n_words, 768]
         # x = F.dropout(x, 0.5)
-        x = self.fc1(x)
+        x = self.fc1(x)  # [N, n_words, 768] -> [N, n_words, 256]
         x = F.relu(x)
         # x = F.dropout(x, 0.5)
-        x = self.fc2(x)
+        x = self.fc2(x)  # [N, n_words, 256] -> [N, n_words, 64]
         x = F.relu(x)
         # x = F.dropout(x, 0.5)
-        x = self.fc3(x)
-        x = F.relu(x)
-        # x = F.dropout(x, 0.5)
-        x = self.fc4(x)
-        x = F.relu(x)
-        # x = F.dropout(x, 0.5)
-        x = self.fc5(x)
+        x = self.fc3(x)  # [N, n_words, 64] -> [N, n_words, 2]
         return x  # [N, n_words, 2]
