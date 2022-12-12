b7f0208486ff314661b276a10021ad86e568499d
Muhwan Kim <mhkim4886@gmail.com>
2022-12-13 04:18:33 +0900

    Remove KoBERT model

--- utils/utils.py
+++ utils/utils.py
@@ -5,16 +5,13 @@ import pickle as pk
 import random
 import sys
 from collections import defaultdict
-from zipfile import ZipFile
 
 import numpy as np
-import sentencepiece as sp
 import torch
 import torch.distributed as dist
 from omegaconf import OmegaConf
 from transformers import BertTokenizerFast, BertModel
 
-from utils.kobert_utils import download, get_tokenizer
 from utils.evaluate import evaluate
 
 
@@ -34,33 +31,6 @@ def get_logger(cfg, name=None):
     if is_logging_process():
         logging.config.dictConfig(OmegaConf.to_container(cfg.job_logging_cfg, resolve=True))
         return logging.getLogger(name)
-
-
-def get_pytorch_kobert_model(ctx="cpu", cachedir=".cache"):
-    def get_kobert_model(model_path, vocab_file, ctx="cpu"):
-        bertmodel = BertModel.from_pretrained(model_path, return_dict=False)
-        device = torch.device(ctx)
-        bertmodel.to(device)
-        bertmodel.eval()
-        vocab = sp.SentencePieceProcessor()
-        vocab.load(vocab_file)
-        return bertmodel, vocab
-
-    pytorch_kobert = {
-        "url": "s3://skt-lsl-nlp-model/KoBERT/models/kobert_v1.zip",
-        "chksum": "411b242919",  # 411b2429199bc04558576acdcac6d498
-    }
-
-    # download model
-    model_info = pytorch_kobert
-    model_path, is_cached = download(model_info["url"], model_info["chksum"], cachedir=cachedir)
-    cachedir_full = os.path.expanduser(cachedir)
-    zipf = ZipFile(os.path.expanduser(model_path))
-    zipf.extractall(path=cachedir_full)
-    model_path = os.path.join(os.path.expanduser(cachedir), "kobert_from_pretrained")
-    # download vocab
-    vocab_path = get_tokenizer()
-    return get_kobert_model(model_path, vocab_path, ctx)
 
 
 def get_multilingual_bert_model(ctx="cpu"):
